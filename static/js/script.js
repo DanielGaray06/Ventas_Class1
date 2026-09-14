1b36d8878d234a3eaf154bab0cafd3578a885bf4
Daniel_Garay <danielfelipegaray2003@gmail.com>
2026-09-14 10:00:28 -0500

    scriptJs_funtionForm

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,21 @@ const inputCodigo = document.getElementById("codigo")
 const inputNombre = document.getElementById("nombre")
 const inputPrecio = document.getElementById("precio")
 const inputCantidad = document.getElementById("cantidad")
-const listaProductos = []
+const listProducts = []
 
 //Agg producto
 
 formulario.addEventListener("submit", function (event){
     event.preventDefault();
-    
+    const NewProduct = {
+        codigo: inputCodigo.value,
+        nombre: inputNombre.value,
+        precio: Number(inputPrecio.value),
+        cantidad: Number(inputCantidad.value),
+        total: 0
+    }
+    listProducts.push(NewProduct)
+    resultContent.style.display = "block";
+    updateTable();
+    form.reset();
 });
