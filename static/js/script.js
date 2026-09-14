f13d47e5288c1ca0b54fb1f4b9b59d05e623db7a
Daniel_Garay <danielfelipegaray2003@gmail.com>
2026-09-14 09:51:37 -0500

    Style_form

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+//Variables 
+const formulario = document.getElementById("formulario")
+const inputCodigo = document.getElementById("codigo")
+const inputNombre = document.getElementById("nombre")
+const inputPrecio = document.getElementById("precio")
+const inputCantidad = document.getElementById("cantidad")
+const listaProductos = []
+
+//Agg producto
+
+formulario.addEventListener("submit", function (event){
+    event.preventDefault();
+    
+});
