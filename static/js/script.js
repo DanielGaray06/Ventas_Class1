443c7e5a42864ffe4689e6c6225c64ab50aa1374
Daniel_Garay <danielfelipegaray2003@gmail.com>
2026-09-14 10:26:44 -0500

    function_updateTable

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,31 @@
+
 //Variables 
 const formulario = document.getElementById("formulario")
 const inputCodigo = document.getElementById("codigo")
 const inputNombre = document.getElementById("nombre")
 const inputPrecio = document.getElementById("precio")
 const inputCantidad = document.getElementById("cantidad")
+const resultContent = document.getElementById("result")
+const tableBody = document.getElementById("tableBody")
 const listProducts = []
 
-//Agg producto
+
+//Agregar producto
+function updateTable(){
+    tableBody.innerHTML = ""
+    listProducts.forEach(product => {
+        const row = document.createElement("tr")
+        row.innerHTML = `
+            <td>${product.codigo}</td>
+            <td>${product.nombre}</td>
+            <td>${product.precio}</td>
+            <td>${product.cantidad}</td>
+            <td>${product.total}</td>
+        `
+        tableBody.appendChild(row)
+    })
+}
+
 
 formulario.addEventListener("submit", function (event){
     event.preventDefault();
@@ -22,3 +41,20 @@ formulario.addEventListener("submit", function (event){
     updateTable();
     formulario.reset();
 });
+
+//Mostrar Productos en la Tabla
+function updateTable(){
+    tableBody.innerHTML = ""
+    listProducts.forEach(function (product) {
+       const row = document.createElement("tr");
+       row.innerHTML = `
+           <td>${product.codigo}</td>
+           <td>${product.nombre}</td>
+           <td>${product.precio}</td>
+           <td>${product.cantidad}</td>
+           <td>${product.total}</td>
+       `
+       tableBody.appendChild(row);
+       
+    })
+}
